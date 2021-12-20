5231f772cd2a198410f8485c0cc6da0c775e631e
Matt Livingston <mlivingston2066@gmail.com>
2021-12-20 16:22:16 -0600

    Fix redirect home issue

--- src/config.js
+++ src/config.js
@@ -2,7 +2,6 @@ const oktaAuthConfig ={
     //This is a comment
     issuer: 'https://dev-84382888.okta.com/oauth2/commemoro',
     clientId: '0oa2vbpeyn400lev55d7',
-    pkce: false,
     redirectUri: window.location.origin + '/login/callback',
 };
 
@@ -12,7 +11,6 @@ const oktaSignInConfig = {
     issuer: 'https://dev-84382888.okta.com/oauth2/commemoro',
     redirectUri: window.location.origin + '/login/callback',
     authParams: {
-        pkce: false,
         baseUrl: 'https://dev-84382888.okta.com',
         issuer: 'https://dev-84382888.okta.com/oauth2/commemoro'
     }
